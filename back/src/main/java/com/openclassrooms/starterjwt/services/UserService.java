70e9c42070bedf87a7cf52f38ec8dc4687787f2d
Fleurdel34 <betdevelweb0953@gmail.com>
2026-01-24 14:23:41 +0100

    update AuthController and AuthService to use the correct segmentation

--- back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
+++ back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
@@ -4,12 +4,19 @@ import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public void createUser(User user){
+        this.userRepository.save(user);
     }
 
     public void delete(Long id) {
@@ -18,5 +25,13 @@ public class UserService {
 
     public User findById(Long id) {
         return this.userRepository.findById(id).orElse(null);
+    }
+
+    public Boolean existsByEmail(String email){
+        return this.userRepository.existsByEmail(email);
+    }
+
+    public Optional<User> findByEmail(String email){
+        return this.userRepository.findByEmail(email);
     }
 }
