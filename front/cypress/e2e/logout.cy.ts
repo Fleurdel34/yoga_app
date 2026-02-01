71c4fb280c1c26526d06be36d24ecdbd75c4a4ff
Fleurdel34 <betdevelweb0953@gmail.com>
2026-02-01 20:50:46 +0100

    one test cypress logout ok

---
+++ front/cypress/e2e/logout.cy.ts
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+
+describe('Log out spec', () => {
+    it('Log out successfull', () => {
+        cy.visit('/login')
+
+        cy.intercept('POST', '/api/auth/login', {
+            body: {
+                id: 1,
+                username: 'userName',
+                firstName: 'firstName',
+                lastName: 'lastName',
+                admin: true
+            },
+        })
+
+        cy.intercept(
+        {
+            method: 'GET',
+            url: '/api/session',
+        },
+        []).as('session')
+
+        cy.get('input[formControlName=email]').type("yoga@studio.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+        cy.url().should('include', '/sessions') 
+
+        cy.get('span').contains('Logout').click()
+
+        cy.url().should('include', '')
+    })
+}) 
