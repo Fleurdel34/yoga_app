f7a3f507fed088bb2da105245011750a57571dbb
Fleurdel34 <betdevelweb0953@gmail.com>
2026-02-06 21:43:01 +0100

    create one file form for createSession and updateSession for coverage 80%

--- front/cypress/e2e/form.cy.ts
+++ front/cypress/e2e/form.cy.ts
@@ -1,6 +1,262 @@
 /// <reference types="cypress" />
 
 describe('Session spec', () => {
+    it('create session, user admin', () => {
+        cy.visit('/login')
+
+        cy.intercept('POST', '/api/auth/login', {
+            body: {
+                id: 1,
+                email:"yoga@studio.com",
+                password:"test!1234",
+                admin: true
+            },
+        })
+
+        cy.get('input[formControlName=email]').type("yoga@studio.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+        cy.intercept(
+        {
+            method: 'GET',
+            url: '/api/session',
+        },
+        [
+            {
+                id: 1,
+                name: "Meditation Session",
+                description: "relaxing meditation session",
+                date: "2025-01-01T00:00:00Z",
+                teacher_id: 1,
+                users: [1, 2, 3],
+                createdAt: "2025-01-01T00:00:00Z",
+                updatedAt: "2025-01-22T00:00:00Z",
+            },
+            {
+                id: 2,
+                name: "Yoga Session",
+                description: "energizing yoga session",
+                date: "2025-01-02T00:00:00Z",
+                teacher_id: 2,
+                users: [4, 5],
+                createdAt: "2025-01-02T00:00:00Z",
+                updatedAt: "2025-02-15T00:00:00Z",
+            },
+        ]).as('session')
+
+        cy.wait('@session')
+
+        cy.get('button').contains('Create').click()
+
+        cy.intercept('POST', '/api/session', {
+            statusCode:201,
+            body: {
+                name: "Meditation",
+                description: "relaxation",
+                date: "2025-03-02T00:00:00Z",
+                teacher_id: 1,
+                users:[],
+                createdAt:"2025-03-02T00:00:00Z",
+                updatedAt: "2025-03-02T00:00:00Z"
+            },
+        }).as('createSession')
+
+        cy.intercept('GET', '/api/teacher',
+        {body: [{
+                id: 1,
+                lastName: "Doe",
+                firstName: "John",
+                createdAt: "2025-01-02T00:00:00Z",
+                updatedAt: "2025-02-15T00:00:00Z",
+            },{
+                id: 2,
+                lastName: "Triolet",
+                firstName: "Elsa",
+                createdAt: "2025-01-02T00:00:00Z",
+                updatedAt: "2025-02-15T00:00:00Z",
+            }],   
+        },).as('teacher')
+
+        cy.wait('@teacher')
+
+        cy.get('input[formControlName=name]').type("Mediation")
+        cy.get('input[formControlName=date]').type("2025-03-02")
+        cy.get('mat-select').eq(0).click()
+        cy.get('mat-option').contains('John Doe').click()
+        cy.get('textarea[formControlName=description]').type("relaxation")
+        cy.get('button[type=submit]').should('not.be.disabled');
+        cy.get('button').contains('Save').click()
+
+        cy.intercept(
+        {
+            method: 'GET',
+            url: '/api/session',
+        },
+        [
+            {
+                id: 1,
+                name: "Meditation Session",
+                description: "relaxing meditation session",
+                date: "2025-01-01T00:00:00Z",
+                teacher_id: 1,
+                users: [1, 2, 3],
+                createdAt: "2025-01-01T00:00:00Z",
+                updatedAt: "2025-01-22T00:00:00Z",
+            },
+            {
+                id: 2,
+                name: "Yoga Session",
+                description: "energizing yoga session",
+                date: "2025-01-02T00:00:00Z",
+                teacher_id: 2,
+                users: [4, 5],
+                createdAt: "2025-01-02T00:00:00Z",
+                updatedAt: "2025-02-15T00:00:00Z",
+            },
+            {
+                id:3,
+                name: "Meditation",
+                description: "energizing yoga session",
+                date: "2025-03-02T00:00:00Z",
+                teacher_id: 1,
+                users: [],
+                createdAt: "2025-03-02T00:00:00Z",
+                updatedAt: "2025-03-02T00:00:00Z",
+            },
+        ]).as('sessionAfterCreateSession')
+
+        cy.wait('@createSession');
+        cy.wait('@sessionAfterCreateSession')
+
+        cy.contains('Session created !').should('exist')
+
+        cy.url().should('include', '/session')
+
+    });
+
+
+    it('create session failed, user admin', () => {
+        cy.visit('/login')
+
+        cy.intercept('POST', '/api/auth/login', {
+            body: {
+                id: 1,
+                email:"yoga@studio.com",
+                password:"test!1234",
+                admin: true
+            },
+        })
+
+        cy.get('input[formControlName=email]').type("yoga@studio.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+        cy.intercept(
+        {
+            method: 'GET',
+            url: '/api/session',
+        },
+        [
+            {
+                id: 1,
+                name: "Meditation Session",
+                description: "relaxing meditation session",
+                date: "2025-01-01T00:00:00Z",
+                teacher_id: 1,
+                users: [1, 2, 3],
+                createdAt: "2025-01-01T00:00:00Z",
+                updatedAt: "2025-01-22T00:00:00Z",
+            },
+            {
+                id: 2,
+                name: "Yoga Session",
+                description: "energizing yoga session",
+                date: "2025-01-02T00:00:00Z",
+                teacher_id: 2,
+                users: [4, 5],
+                createdAt: "2025-01-02T00:00:00Z",
+                updatedAt: "2025-02-15T00:00:00Z",
+            },
+        ]).as('session')
+
+        cy.wait('@session')
+
+        cy.get('button').contains('Create').click()
+
+        cy.intercept('GET', '/api/teacher',
+        {body: [{
+                id: 1,
+                lastName: "Doe",
+                firstName: "John",
+                createdAt: "2025-01-02T00:00:00Z",
+                updatedAt: "2025-02-15T00:00:00Z",
+            },{
+                id: 2,
+                lastName: "Triolet",
+                firstName: "Elsa",
+                createdAt: "2025-01-02T00:00:00Z",
+                updatedAt: "2025-02-15T00:00:00Z",
+            }],   
+        },).as('teacher')
+
+        cy.wait('@teacher')
+
+        cy.get('input[formControlName=name]').focus().blur()
+        cy.get('input[formControlName=date]').focus().blur()
+        cy.get('mat-select').eq(0).click()
+        cy.get('mat-option').contains('John Doe').click()
+        cy.get('textarea[formControlName=description]').focus().blur()
+        cy.get('button[type=submit]').should('be.disabled')
+    });
+
+    it('user not admin redirect to sessions', () => {
+        cy.visit('/login')
+
+        cy.intercept('POST', '/api/auth/login', {
+            body: {
+                id: 1,
+                email:"yoga@studio.com",
+                password:"test!1234",
+                admin: false
+            },
+        })
+
+        cy.get('input[formControlName=email]').type("yoga@studio.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+        cy.intercept(
+        {
+            method: 'GET',
+            url: '/api/session',
+        },
+        [
+            {
+                id: 1,
+                name: "Meditation Session",
+                description: "relaxing meditation session",
+                date: "2025-01-01T00:00:00Z",
+                teacher_id: 1,
+                users: [1, 2, 3],
+                createdAt: "2025-01-01T00:00:00Z",
+                updatedAt: "2025-01-22T00:00:00Z",
+            },
+            {
+                id: 2,
+                name: "Yoga Session",
+                description: "energizing yoga session",
+                date: "2025-01-02T00:00:00Z",
+                teacher_id: 2,
+                users: [4, 5],
+                createdAt: "2025-01-02T00:00:00Z",
+                updatedAt: "2025-02-15T00:00:00Z",
+            },
+        ]).as('session')
+
+        cy.wait('@session')
+
+        cy.url().should('include', '/sessions')
+
+    });
+
     it('update session, user admin', () => {
         cy.visit('/login')
 
@@ -258,4 +514,4 @@ describe('Session spec', () => {
 
     });
 
-}) 
+})  
